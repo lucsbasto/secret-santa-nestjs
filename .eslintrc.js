b472edb19f3f4d013dd5fe2a0d7cfcdc327f7d75
Lucas Bastos <lucsbasto@gmail.com>
2020-09-09 17:08:38 -0300

    add raffle service

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,78 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
+    ecmaVersion: 11,
     project: 'tsconfig.json',
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: [
+    '@typescript-eslint/eslint-plugin',
+    '@typescript-eslint',
+    'eslint-plugin-import-helpers',
+  ],
   extends: [
-    'plugin:prettier/recommended',
+    'standard',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
-    'prettier',
     'prettier/@typescript-eslint',
   ],
   root: true,
   env: {
+    es2020: true,
     node: true,
     jest: true,
   },
   rules: {
+    semi: [2, 'always', { omitLastInOneLineBlock: true }],
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'class-methods-use-this': 'off',
+    'no-param-reassign': 'off',
+    'no-new': 'off',
+    'no-prototype-builtins': 'off',
+    'no-restricted-syntax': 'off',
+    'max-classes-per-file': 'off',
+    'no-console': 'off',
+    'import/prefer-default-export': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '_',
+      },
+    ],
+    'no-useless-constructor': 'off',
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'interface',
+        format: ['PascalCase'],
+        custom: {
+          regex: '^I[A-Z]',
+          match: true,
+        },
+      },
+    ],
+    '@typescript-eslint/explicit-module-boundary-types': ['warn'],
+    'no-underscore-dangle': 'off',
+    'import-helpers/order-imports': [
+      'warn',
+      {
+        newlinesBetween: 'always', // new line between groups
+        groups: [
+          'module',
+          '/^@server/shared/',
+          '/^@/',
+          ['parent', 'sibling', 'index'],
+        ],
+        alphabetize: {
+          order: 'asc',
+          ignoreCase: true,
+        },
+      },
+    ],
   },
 };
